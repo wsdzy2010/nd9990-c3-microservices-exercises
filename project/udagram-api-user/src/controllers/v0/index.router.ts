9d349eb33514229f6b44f70c5f7aebb558bb0754
zhenyuan du <zdu12@hawk.iit.edu>
2020-08-11 23:04:07 -0500

    fix: users endpoint

--- project/udagram-api-user/src/controllers/v0/index.router.ts
+++ project/udagram-api-user/src/controllers/v0/index.router.ts
@@ -3,7 +3,7 @@ import {UserRouter} from './users/routes/user.router';
 
 const router: Router = Router();
 
-router.use('/', UserRouter);
+router.use('/users', UserRouter);
 
 router.get('/', async (req: Request, res: Response) => {
   res.send(`V0`);
